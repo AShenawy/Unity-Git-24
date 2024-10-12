afc68c6f8994112ad2128451016f4aff656a838b
domy838 <domy@tlu.ee>
2024-10-12 10:36:24 +0300

    Change code in GitClass.cs
    Do challenge.

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -6,6 +6,7 @@ public class GitClass : MonoBehaviour
 {
     string myName = "Git";
     int myPower = 9001;
+    int powerReduction = 50;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +16,17 @@ public class GitClass : MonoBehaviour
         if (myPower > 9000)
         {
             print("It's over 9000!!!");
+            myPower -= powerReduction;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(myPower > 8000)
+        {
+            print("My power is still greater than 8000");
+            myPower -= powerReduction;
+        }
     }
 }
