32fac7b82ae1fc2eba5995f5b4e88ec207992255
Geraldine1610 <geraldine.sch2027@gmail.com>
2024-10-13 15:11:26 +0300

    Do challenge
    Change number 9001 to 9016. Change first print "My name is" to "Minu nimi on". Add string myPic and print.

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class GitClass : MonoBehaviour
 {
     string myName = "Git";
-    int myPower = 9001;
+    int myPower = 9016;
+    string myPic = "tornado";
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello! My name is " + myName);
+        print("Hello! Minu nimi on " + myName);
 
         if (myPower > 9000)
         {
             print("It's over 9000!!!");
         }
+
+        print("I added the picture " + myPic);
     }
 
     // Update is called once per frame
