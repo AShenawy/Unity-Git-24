cd57b63e9883e90744f427cc52e55d9fa2fe8a55
crismo04 <59480079+crismo04@users.noreply.github.com>
2024-10-17 18:19:47 +0300

    Do challenge

--- Assets/Scripts/GitClass.cs
+++ Assets/Scripts/GitClass.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class GitClass : MonoBehaviour
 {
     string myName = "Git";
-    int myPower = 9001;
+    int myPower = 9000;
+    int myPoints = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         print("Hello! My name is " + myName);
 
+        myPower++;
         if (myPower > 9000)
         {
             print("It's over 9000!!!");
+            print("It's " + myPower + ", and your points are " + myPoints);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (myPoints < 9000)
+                myPoints++;
+        else if (myPoints == 9000)
+        {
+            print("It's over 9000!!!");
+            myPoints++;
+        }
     }
 }
